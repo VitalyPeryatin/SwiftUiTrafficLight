3869ec7865607a986e964446181239c2b9e29e70
VitalyPeryatin <peryatin.vitalik37@gmail.com>
2021-03-26 09:38:43 +0300

    Initial Commit

---
+++ SwiftUiTrafficLight/SwiftUiTrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiTrafficLightApp.swift
+//  SwiftUiTrafficLight
+//
+//  Created by Виталий on 26.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiTrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
